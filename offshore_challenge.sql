ae51ba16f59115d558864936849d95835e87c4fb
Tolga Oztan <tolgaoztan@gmail.com>
2016-04-10 00:59:39 -0400

    Results finalized with 6 questions in total

--- offshore_challenge.sql
+++ offshore_challenge.sql
@@ -35,8 +35,17 @@ SELECT CAST((SELECT (SELECT COUNT(Unique_ID) FROM edges_1DNW)*2) /
 	(SELECT (SELECT (SELECT COUNT(Unique_ID) FROM nodesNW) - 1) * (SELECT COUNT(Unique_ID) FROM nodesNW)) AS decimal(10,9))
 	 AS density;
 
+## WHICH COMPANIES/INDIVIDUALS ARE INVOLVED THE MOST?
+
+SELECT Description_, COUNT(*) AS counts FROM nodesNW
+GROUP BY Description_
+ORDER BY counts DESC
+LIMIT 20;
 
 
+
+## table for the nodes for which the edges are going into. (their country names and
+## the description of the edge going into them.)
 SELECT node1.NODEID1, node1.country_name, edge1.Unique_ID, edge1.Entity_ID1, edge1.description_  
 FROM (SELECT Unique_ID, Entity_ID1, Entity_ID2, description_ 
 FROM edges_1DNW
@@ -104,11 +113,23 @@ LIMIT 20;*/
 ## HOW MANY DISTINCT POSITIONS/ROLES CAN EDGES HAVE?
 SELECT DISTINCT(description_) FROM edges_1DNW;
 
-
-SELECT Entity_ID1, COUNT(*) AS counts FROM edges_1DNW
+# Which Individuals/Companies Have the most out-going edges and 
+# which countries they operate in?
+SELECT Entity_ID1, counts, Description_, country_name
+FROM(
+SELECT Entity_ID1, counts, Description_ 
+FROM (SELECT Entity_ID1, COUNT(*) AS counts FROM edges_1DNW
+WHERE direction = 1
 GROUP BY Entity_ID1
 ORDER BY counts DESC
-LIMIT 20;
+LIMIT 20) AS entities
+LEFT JOIN nodesNW AS node
+ON entities.Entity_ID1 = node.Unique_ID
+LIMIT 10)AS entities2
+LEFT JOIN node_countriesNW AS countries
+ON entities2.Entity_ID1 = countries.NODEID1
+ORDER BY entities2.counts DESC;
+
 
 SELECT Entity_ID2, COUNT(*) AS counts FROM edges_1DNW
 GROUP BY Entity_ID2
